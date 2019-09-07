8cee9481a5bb68a192bb74e66a6fc8324e170285
atpure <atpure100@gmail.com>
2019-09-08 01:50:04 +0900

    Finish

--- arduino_temperature_alert.ino
+++ arduino_temperature_alert.ino
@@ -8,15 +8,6 @@
 #define BUZZER 16 // D0
 #define EEPROM_SIZE 1
 
-
-// size of buffer used to capture HTTP requests
-#define REQ_BUF_SZ   90
-// size of buffer that stores the incoming string
-#define TXT_BUF_SZ   50
-char HTTP_req[REQ_BUF_SZ] = {0}; // buffered HTTP request stored as null terminated string
-char req_index = 0;              // index into HTTP_req buffer
-char txt_buf[TXT_BUF_SZ] = {0};  // buffer to save text to
-
 int print_interval = 15; // x 200ms
 int interval_count = print_interval;
 double alert_temperature = 30;
@@ -50,7 +41,7 @@ void setup() {
   EEPROM.begin(EEPROM_SIZE);
   val = EEPROM.read(address);
 
-  if (val > 0 && val < 50) {
+  if (val > 0 && val <= 50) {
     alert_temperature = val;
   }
   Serial.print("Alert Temperature: ");
@@ -64,55 +55,47 @@ void loop() {
   WiFiClient client = server.available();
 
   if (client) {
-    Serial.println("new client");   
-    // an http request ends with a blank line
-    bool currentLineIsBlank = true;
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        Serial.write(c);      
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the http request has ended,
-        // so you can send a reply                
-        if (c == '\n' && currentLineIsBlank) {
-          // send a standard http response header
+    Serial.println("new client");
+
+    String req = client.readStringUntil('\r');
+    Serial.println(req);
+    client.flush();
+    
+    if (req.indexOf("/?alert_temp") > 0) {
+      String newVal = req.substring(req.indexOf("=")+1, req.indexOf("HTTP")-1);
+      Serial.println(newVal);
+      SaveValue(newVal.toInt());
+    }
+
+
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
+//          client.println("Connection: close");  // the connection will be closed after completion of the response
           client.println("Refresh: 5");  // refresh the page automatically every 5 sec
           client.println();
           client.println("<!DOCTYPE HTML>");
           client.println("<html>");
+          client.println("<body>");
           client.print("Alert Temperature: ");
           client.println(alert_temperature);
           client.println("<br>");
           client.println(F("  <form method=\"get\">"));
-          client.print(F("  <input type='text' name='alert_temp' id='alert_temp' size=2 autofocus> "));
-          client.println(F(" <input type=\"button\" name=\"button\" value=\"SET\" onclick=\"SaveValue();\">"));
+          client.println(F("  <input type='text' name='alert_temp' id='alert_temp' size=2 autofocus> "));
+          client.println(F(" <input type=\"button\" name=\"button\" value=\"SET\" onclick=\"submit();\">"));
           client.println(F("  </form>"));
+          client.println("(1 ~ 50)");
           client.println("<br>");
           client.println("<h1>Temperature</h1>");
-          client.print(tempO);
+          client.println(tempO);
           client.println("<br>");
+          client.println("</body>");
           client.println("</html>");                    
-          break;
-        }
-        if (c == '\n') {
-          // you're starting a new line
-          currentLineIsBlank = true;
-        } else if (c != '\r') {
-          // you've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
-      }
-    }
+
     // give the web browser time to receive the data
     delay(1);
-    // close the connection:
-    client.stop();
     Serial.println("client disconnected");
   }
-  
+
 
   if (--interval_count < 1 ) {
     interval_count = print_interval;
@@ -125,7 +108,7 @@ void loop() {
         digitalWrite(BUZZER, LOW);
         buzz = true;
     } else {
-        digitalWrite(BUZZER, LOW);
+        digitalWrite(BUZZER, HIGH);
         buzz = false;
     }
     delay(200);
@@ -134,5 +117,15 @@ void loop() {
     digitalWrite(BUZZER, LOW);
     buzz = false;
     delay(200);
+  }
+}
+
+void SaveValue(int temp)
+{
+  if (temp > 0 && temp <= 50) {
+    Serial.println("New Value Saved");
+    alert_temperature = temp;
+    EEPROM.write(address, temp);
+    EEPROM.commit();
   }
 }
