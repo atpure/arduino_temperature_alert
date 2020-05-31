84249d71b85d834dc51c348845420e3c571e7988
atpure <atpure100@gmail.com>
2020-05-31 23:40:18 +0900

    add oled

--- arduino_temperature_alert.ino
+++ arduino_temperature_alert.ino
@@ -4,6 +4,15 @@
 #include <WiFiClient.h>
 //#include <ESP8266WebServer.h>
 #include <EEPROM.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 #define BUZZER 16 // D0
 #define EEPROM_SIZE 1
@@ -14,9 +23,9 @@ int high_temp_check = 5; // x 200ms
 int high_temp_check_count = high_temp_check;
 int led_interval = 5; // x 200ms
 int led_interval_count = led_interval;
-double alert_temperature = 30;
-double recovery_threthold = 1;
-double tempA, tempO, tempO_max;
+float alert_temperature = 30;
+float recovery_threthold = 1;
+float tempA, tempO, tempO_max;
 boolean high_temp = false;
 boolean alert_on = false;
 boolean buzz_on = false;
@@ -54,6 +63,11 @@ void setup() {
   }
   Serial.print("Alert Temperature: ");
   Serial.println(alert_temperature);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+  }
 }
 
 void loop() {
@@ -72,13 +86,13 @@ void loop() {
     String req = client.readStringUntil('\r');
     Serial.println(req);
     client.flush();
-    
+
     if (req.indexOf("/?alert_temp") > 0) {
       String newVal = req.substring(req.indexOf("=")+1, req.indexOf("HTTP")-1);
       Serial.println(newVal);
       SaveValue(newVal.toInt());
     }
-    
+
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
 //          client.println("Connection: close");  // the connection will be closed after completion of the response
@@ -105,7 +119,7 @@ void loop() {
           client.print("<h2>");
           client.println("<br>");
           client.println("</body>");
-          client.println("</html>");                    
+          client.println("</html>");
 
     // give the web browser time to receive the data
     delay(1);
@@ -117,7 +131,7 @@ void loop() {
     print_interval_count = print_interval;
     Serial.print("Object Temperature = ");Serial.println(tempO);
   }
-  
+
   if (tempO > alert_temperature || (alert_on && (tempO > alert_temperature - recovery_threthold))) {
     digitalWrite(LED_BUILTIN, LOW);
     high_temp = true;
@@ -158,12 +172,53 @@ void loop() {
           led_interval_count = led_interval;
       }
     }
- 
+
     high_temp_check_count = high_temp_check;
     digitalWrite(BUZZER, LOW);
     buzz_on = false;
     delay(200);
   }
+
+  // OLDE Display
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,4);
+  display.println("Target");
+
+  display.setTextSize(2);
+  display.setCursor(50,0);
+  display.println(tempO,1);
+
+  display.setCursor(110,0);
+  display.println("C");
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,17);
+  display.println("Max");
+
+  display.setTextSize(1);
+  display.setCursor(50,17);
+  display.println(tempO_max,1);
+
+  display.setCursor(110,17);
+  display.println("C");
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,25);
+  display.println("Alert");
+
+  display.setTextSize(1);
+  display.setCursor(50,25);
+  display.println(alert_temperature,1);
+
+  display.setCursor(110,25);
+  display.println("C");
+
+  display.display();
 }
 
 void SaveValue(int temp)
